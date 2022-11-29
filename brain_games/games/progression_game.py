f03017701fe36b186116e6d598fce056f6df0b7f
Евгений Яковлев <eugene.yakovlev@rambler.ru>
2022-11-29 18:05:58 +0700

    add function in progression

--- brain_games/games/progression_game.py
+++ brain_games/games/progression_game.py
@@ -1,21 +1,25 @@
 "игра прогрессия"
 from random import randint
 
-
 GAME_ANNOUNCE = 'What number is missing in the progression?'
 
 
 def game():
-    step = randint(1, 10)  # выставляем шаг между числами
-    start_digit = randint(1, 10)  # начальное число
-    digit_list = [start_digit]
-    for i in range(1, 11):
-        new_digit = start_digit + step
-        digit_list.extend([new_digit])
-        start_digit = new_digit
     random_index = randint(1, 10)
-    result = str(digit_list[random_index])
-    digit_list[random_index] = '..'
-    digit_string = str(digit_list).strip('[]').replace("'", "").replace(",", "")
-    question = str(f'Question: {digit_string}')
+    number_list = progression()
+    result = str(number_list[random_index])
+    number_list[random_index] = '..'
+    string = str(number_list).strip('[]').replace("'", "").replace(",", "")
+    question = str(f'Question: {string}')
     return result, question
+
+
+def progression():
+    step = randint(1, 10)  # выставляем шаг между числами
+    start_number = randint(1, 10)  # начальное число
+    list = [start_number]
+    for i in range(1, 11):
+        append_number = start_number + step
+        list.append(append_number)
+        start_number = append_number
+    return list
